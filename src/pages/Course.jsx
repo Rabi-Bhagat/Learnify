23ca3a902444a3d9015d04b80f0793e2a15a47b1
Rabi Bhagat <rabibhagat789@gmail.com>
2025-10-18 16:42:44 +0530

    add back navigation icon to Course component

--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Dashboard_header";
 import Footer from "../components/Footer";
-
+import { FaArrowLeft } from "react-icons/fa";
 const coursesData = [
   {
     title: "Conversational Spanish",
